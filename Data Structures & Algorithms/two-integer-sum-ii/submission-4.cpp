92b08a48d88b833936b03bde919f42f6d65ab076
zemplenyib <78876124+zemplenyib@users.noreply.github.com>
2026-04-28 10:55:55 +0200

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        std::unordered_map<int, int> mp;
+        for (auto& n : numbers) {
+            auto it = mp.find(target-n);
+            if (it != mp.end()) {
+                int idx1 = it->second;
+                int idx2 = &n - &numbers[0] + 1;
+                return {idx1, idx2};
+            } else {
+                mp[n] = &n - &numbers[0] + 1;
+            }
+        }
+    }
+};
